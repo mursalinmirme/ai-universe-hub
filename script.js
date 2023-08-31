f83978b1509354d80c4f4f080d08bc5a79806f41
mursalinmirme <mursalinmir02@gmail.com>
2023-08-31 10:58:45 +0600

    I have update something

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const showAllAiTools = async () => {
     // console.log(allAi);
     const itemContainer = document.getElementById('itemContainer');
     allAi = allAi.slice(0, 6)
-    console.log(jasparChat)
     for(const item of allAi){
         const div = document.createElement('div');
         div.classList = `card card-compact bg-base-100 p-4 border`;
@@ -33,7 +32,7 @@ const showAllAiTools = async () => {
                         </div>
                       </div>
                       <div>
-                        <button>
+                        <button onclick="showItemModal('${item?.id}')">
                         <i class="fa-solid fa-arrow-right text-orange-400 bg-orange-50 px-4 py-3.5 rounded-full"></i>
                         </button>
                       </div>
@@ -47,5 +46,81 @@ const showAllAiTools = async () => {
 
 showAllAiTools();
 
-
+// show signle item modal
+const showItemModal = async (modalId) => {
+    const modalUrl = `https://openapi.programming-hero.com/api/ai/tool/${modalId}`;
+    const response = await fetch(modalUrl);
+    const itemData = await response.json();
+    const getData = itemData.data;
+    // if 6 data image is undefined
+    if(getData.id === '06'){
+      getData.image_link[0] = 'jaspar-chat.jpg';
+    }
+    // catch the accuracy
+    const accur = getData.accuracy.score;
+    const accurString = accur.toString();
+    const getAccur = accurString.split(".");
+    const concurrency = getAccur[1];
+    console.log(accurString)
+    const modalContainer = document.getElementById('modalContainer');
+    modalContainer.innerHTML = `
+    <form method="dialog" class="">
+                        <!-- modal wrapper -->
+          <div class="flex gap-4 bg-white p-20 relative rounded-xl">
+            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 text-white bg-red-500">✕</button>
+            <!-- discription -->
+            <div class="flex-1 shadow border border-red-500 bg-red-50 rounded-xl p-5">
+              <h3 class="text-lg font-semibold">${getData?.description}</h3>
+              <!-- subscription packege -->
+              <div class="flex gap-4 mt-5">
+                <div class="py-3 text-center flex flex-col items-center justify-center w-1/3 text-base font-semibold text-green-700 rounded-xl bg-white">
+                 <span>${getData?.pricing[0]?.price}</span>
+                 <span>${getData?.pricing[0]?.plan}</span>
+                </div>
+                <div class="py-3 text-center flex flex-col items-center justify-center w-1/3 text-base font-semibold text-orange-500 rounded-xl bg-white">
+                <span>${getData?.pricing[1]?.price}</span>
+                <span>${getData?.pricing[1]?.plan}</span>
+                </div>
+                <div class="py-3 text-center flex flex-col items-center justify-center w-1/3 text-sm font-semibold text-red-500 rounded-xl bg-white">
+                <span>${getData?.pricing[2]?.price}</span>
+                <span>${getData?.pricing[2]?.plan}</span>
+                </div>
+              </div>
+              <!-- features and integrations container -->
+              <div class="flex justify-between gap-4 mt-5">
+                <!-- feature -->
+                <div>
+                  <h2 class="text-xl font-semibold">Features</h2>
+                  <ul class="text-sm text-gray-600 list-disc pl-5 mt-3">
+                    <li>${getData?.features["1"]?.feature_name}</li>
+                    <li>${getData?.features["2"]?.feature_name}</li>
+                    <li>${getData?.features["3"]?.feature_name}</li>
+                  </ul>
+                </div>
+                <!-- integrations -->
+                <div>
+                  <h2 class="text-xl font-semibold">Integrations</h2>
+                  <ul class="text-sm text-gray-600 list-disc pl-5 mt-3">
+                    <li>${getData?.integrations[0]}</li>
+                    <li>${getData?.integrations[1]}</li>
+                    <li>${getData?.integrations[2]}</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <!-- image and title -->
+            <div class="flex-1 shadow rounded-xl p-5">
+              <div class="relative">
+                <img class="rounded-xl w-full h-60" src="${getData?.image_link[0]}" alt="">
+                <span class="absolute top-2 right-2 bg-red-500 rounded-lg px-3 py-1 text-white text-sm font-medium">${concurrency}% accuracy</span>
+              </div>
+              <h2 class="text-center text-xl font-semibold py-4">${getData.input_output_examples[0].input}</h2>
+              <p class="text-center text-sm px-3">${getData.input_output_examples[0].output}</p>
+            </div>
+          </div>
+            </form>
+    `
+    // show modal
+    showModal.showModal()
+}
 
